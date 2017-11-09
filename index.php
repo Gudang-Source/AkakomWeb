d3f6d9c49d5c43bcf4beb7c08481759f16f14009
Rino Ridlo Julianto <rinoridlojulianto@gmail.com>
2017-11-09 20:37:12 +0700

    Major edit. First commit

--- index.php
+++ index.php
@@ -1,47 +1,100 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title>Akakom</title>
+	<title>STMIK Akakom</title>
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
   <link rel="stylesheet"  href="css/bootstrap.css">
 </head>
 <body>
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <a class="navbar-brand" href="#">Navbar</a>
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
+	<nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+	  <a class="navbar-brand" href="#">STMIK Akakom</a>
+	  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+	    <span class="navbar-toggler-icon"></span>
+	  </button>
+	  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+	    <ul class="navbar-nav mr-auto">
+	      <li class="nav-item active">
+	        <a class="nav-link" href="index.php">Beranda <span class="sr-only">(current)</span></a>
+	      </li>
+	      <li class="nav-item dropdown">
+	        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+	          Profil Akakom
+	        </a>
+	        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+	          <a class="dropdown-item" href="#">Visi dan Misi</a>
+	          <a class="dropdown-item" href="#">Profil STMIK Akakom</a>
+						<a class="dropdown-item" href="#">Kontak Kami</a>
+						<a class="dropdown-item" href="#">Pejabat di STMIK Akakom</a>
+						<a class="dropdown-item" href="#">Kontak Dosen STMIK Akakom</a>
+	        </div>
+	      </li>
+				<li class="nav-item dropdown">
+	        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+	          Program Studi/ Jurusan
+	        </a>
+	        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+	          <a class="dropdown-item" href="#">Komputerisasi Akuntansi (D3)</a>
+	          <a class="dropdown-item" href="#">Teknik Komputer (D3) </a>
+						<a class="dropdown-item" href="#">Manajemen Informatika (D3)</a>
+						<a class="dropdown-item" href="#">Sistem Informasi (S1)</a>
+						<a class="dropdown-item" href="#">Teknik Informatika (S1)</a>
+	        </div>
+	      </li>
+				<li class="nav-item">
+	        <a class="nav-link" href="#">Info Kampus</a>
+	      </li>
+				<li class="nav-item">
+	        <a class="nav-link" href="#">Berita</a>
+	      </li>
+				<li class="nav-item">
+	        <a class="nav-link" href="#">Arsip Umum</a>
+	      </li>
+	    </ul>
 
-  <div class="collapse navbar-collapse" id="navbarSupportedContent">
-    <ul class="navbar-nav mr-auto">
-      <li class="nav-item active">
-        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#">Link</a>
-      </li>
-      <li class="nav-item dropdown">
-        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-          Dropdown
-        </a>
-        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-          <a class="dropdown-item" href="#">Action</a>
-          <a class="dropdown-item" href="#">Another action</a>
-          <div class="dropdown-divider"></div>
-          <a class="dropdown-item" href="#">Something else here</a>
-        </div>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link disabled" href="#">Disabled</a>
-      </li>
-    </ul>
-    <form class="form-inline my-2 my-lg-0">
-      <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-      <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-    </form>
-  </div>
-</nav>
+	    <form class="form-inline my-2 my-lg-0">
+	      <input class="form-control mr-sm-2" type="search" placeholder="Cari" aria-label="Search">
+	      <button class="btn btn-outline-light my-2 my-sm-0" type="submit">Cari</button>
+	    </form>
+	  </div>
+	</nav>
 
+	<div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+	  <ol class="carousel-indicators">
+	    <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+	    <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+	    <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+	  </ol>
+	  <div class="carousel-inner">
+	    <div class="carousel-item active">
+	      <img class="d-block w-100" src="img/gedung.jpg" alt="First slide">
+	    </div>
+	    <div class="carousel-item">
+	      <img class="d-block w-100" src="img/owasp.jpg" alt="Second slide">
+				<div class="carousel-caption d-none d-md-block">
+			    <h3>OWASP</h3>
+			    <p>Event OWASP yang dilaksanakan di STMIK Akakom</p>
+			  </div>
+	    </div>
+	    <div class="carousel-item">
+	      <img class="d-block w-100" src="img/pika.jpg" alt="Third slide">
+				<div class="carousel-caption d-none d-md-block">
+			    <h3>Ruang Presentasi</h3>
+			    <p>STMIK Akakom menyediakan fasilitas ruang presentasi yang nyaman</p>
+			  </div>
+	    </div>
+	  </div>
+	  <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+	    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+	    <span class="sr-only">Previous</span>
+	  </a>
+	  <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+	    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+	    <span class="sr-only">Next</span>
+	  </a>
+	</div>
+
+	<script src="js/jquery-3.2.1.min.js"></script>
+	<script src="js/popper.js"></script>
   <script src="js/bootstrap.min.js"></script>
 </body>
 </html>
