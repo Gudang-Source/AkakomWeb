2de79f1ea13d2145bd1bf947bdb02890b2197f3c
Muhammad Kodrat <muhammadkodrat@icloud.com>
2017-11-11 02:08:05 +0700

    profile

---
+++ profile.php
@@ -0,0 +1,106 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>STMIK Akakom</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <link rel="stylesheet"  href="css/bootstrap.css">
+</head>
+<body>
+	<nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+	  <a class="navbar-brand" href="#">STMIK Akakom</a>
+	  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+	    <span class="navbar-toggler-icon"></span>
+	  </button>
+	  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+	    <ul class="navbar-nav mr-auto">
+	      <li class="nav-item active">
+	        <a class="nav-link" href="index.php">Beranda <span class="sr-only">(current)</span></a>
+	      </li>
+	      <li class="nav-item dropdown">
+	        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+	          Profil Akakom
+	        </a>
+	        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+	          <a class="dropdown-item" href="visimisi.php">Visi dan Misi</a>
+	          <a class="dropdown-item" href="#">Profil STMIK Akakom</a>
+						<a class="dropdown-item" href="#">Kontak Kami</a>
+						<a class="dropdown-item" href="#">Pejabat di STMIK Akakom</a>
+						<a class="dropdown-item" href="#">Kontak Dosen STMIK Akakom</a>
+	        </div>
+	      </li>
+				<li class="nav-item dropdown">
+	        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+	          Program Studi/ Jurusan
+	        </a>
+	        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+	          <a class="dropdown-item" href="#">Komputerisasi Akuntansi (D3)</a>
+	          <a class="dropdown-item" href="#">Teknik Komputer (D3) </a>
+						<a class="dropdown-item" href="#">Manajemen Informatika (D3)</a>
+						<a class="dropdown-item" href="#">Sistem Informasi (S1)</a>
+						<a class="dropdown-item" href="#">Teknik Informatika (S1)</a>
+	        </div>
+	      </li>
+				<li class="nav-item">
+	        <a class="nav-link" href="#">Info Kampus</a>
+	      </li>
+				<li class="nav-item">
+	        <a class="nav-link" href="#">Berita</a>
+	      </li>
+				<li class="nav-item">
+	        <a class="nav-link" href="#">Arsip Umum</a>
+	      </li>
+	    </ul>
+
+	    <form class="form-inline my-2 my-lg-0">
+	      <input class="form-control mr-sm-2" type="search" placeholder="Pencarian" aria-label="Search">
+	      <button class="btn btn-outline-light my-2 my-sm-0" type="submit">Cari</button>
+	    </form>
+	  </div>
+	</nav>
+<br><br>
+    <div class="container">
+    <h2 class="text-center">Profile STMIK AKAKOM</h2>
+    <br>
+        <div class="row">
+        
+            
+            <div class="col-lg-4">
+                    
+                    <img src="img/freeport.jpg" class="img-fluid" alt="Image">
+                    <img src="img/kuliahtamu.jpg" class="img-fluid" alt="Image">
+                    <img src="img/niit.jpg" class="img-fluid" alt="Image">
+                    
+            </div>
+            <div class="col">
+            
+                    <p class="text-justify">Dalam abad ke-20 ini dunia banyak diwarnai dengan berbagai kemajuan secara menakjubkan yang telah diciptakan oleh umat manusia, baik dibidang ilmu maupun teknologi. Menanggapi kondisi zaman seperti tersebut di atas, pada tanggal 30 Juni 1979 didirikan sebuah yayasan dengan nama Yayasan Pendidikan Widya Bakti, yang bertujuan mengembangkan dan menyebarluaskan informatika dan teknologi komputer di kalangan masyarakat indonesia melalui usaha pendidikan yang sistematis dan ilmiah. Yayasan tersebut mengelola sebuah akademi yang bernama Akademi Aplikasi Komputer, disingkat AKAKOM.</p>
+            
+            
+                    <p class="text-justify">Terhitung mulai 1 Maret 1983, Akademi Aplikasi Komputer(AKAKOM), diubah menjadi Akademi Komputer dan Informatika AKAKOM. Selanjutnya terhitung mulai tanggal 2 Mei 1985, nama Akademi Komputer dan Informatika AKAKOM diubah dan dibakukan menjadi Akademi Manajemen Informatika dan Komputer(AMIK)AKAKOM.</p>
+                    <p class="text-justify">Agar lembaga tersebut mampu menghasilkan tenaga-tenaga profesional maupun akademik yang lebih berbobot dalam bidang informatika dan komputer, maka sejak tanggal 8 Juni 1992, berdasarkan Surat Keputusan Direktorat Jendral Pendidikan Tinggi Departemen Pendidikan dan Kebudayaan RI Nomor 262/DIKTI/Kep/1992, AMIK AKAKOM diubah bentuknya menjadi Sekolah Tinggi Manajemen Informatika dan Komputer (STMIK) AKAKOM yang membuka program sarjana dan program diploma.</p>
+                    <p class="text-justify">Saat ini program sarjana mempunyai program stusi Teknik Informatika dan Sistem Informasi dengan status akreditasi B dan pada program diploma mempunyai 3 program studi yaitu Manajemen Informatika (status Akreditasi B), Teknik Komputer(status Akreditasi B), dan Komputerisasi Akuntansi (status Akreditasi B).</p>
+                    </div>
+        
+        </div>
+
+    </div>
+
+
+		<br>
+		<div class="jumbotron jumbotron-fluid">
+			<div class="container">
+				<h4 class="display-12 text-danger">Yang Pertama dan Utama</h4>
+			  <p class="lead" align="justify">
+			  	Yayasan Pendididkan Widya Bakti berdiri dengan Akta Notaris Nomor 43 tanggal 30 Juni 1979, sebagai penyelenggara pendidikan tinggi dibidang komputer yang pertama di Jateng dan DIY dengan nama Akademi Aplikasi Komputer (AKAKOM). Tahun 1985 AKAKOM menjadi AMIK AKAKOM dan akhirnya sejak tahun 1992 menjadi STMIK AKAKOM.
+			  </p>
+			  <hr class="my-4">
+			  <img class="img-fluid" style="width:100%" src="img/jurusan.png">
+			</div>
+		</div>
+	</div>
+
+	<script src="js/jquery-3.2.1.min.js"></script>
+	<script src="js/popper.js"></script>
+  <script src="js/bootstrap.min.js"></script>
+</body>
+</html>
