6026c6817e8a08a287d7c8d4b544bbff19307d02
Muhammad Kodrat <muhammadkodrat@icloud.com>
2017-11-13 11:13:28 +0700

    Detail Berita

--- detailberita.php
+++ detailberita.php
@@ -68,21 +68,14 @@
 	<div class="container">
 		<div class="row">
 			<div class="col">
-				<h2 style="margin-top:1em; margin-bottom:0.5em">Berita</h2>
+				<h2 style="margin-top:1em; margin-bottom:0.5em">Judul Berita</h2>
 				<div class="card" style="margin-bottom:1em;">
 					<div class="card-block" style="padding:1em; ">
-						<h4 class="card-title">Judul</h4>
-						<p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-						<a href="#" class="btn btn-primary">Go somewhere</a>
+						<p class="card-title">Author, Tanggal</p>
+						<p class="card-text text-justify">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
 					</div>
 				</div>
-				<div class="card" style="margin-bottom:1em;">
-					<div class="card-block" style="padding:1em; ">
-						<h4 class="card-title">Judul</h4>
-						<p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-						<a href="#" class="btn btn-primary">Go somewhere</a>
-					</div>
-				</div>
+
 			</div>
 
 			<div class="col-sm-3">
